5746a5a23bcc1d518c7cfe75d41842260b669a60
Himanshu Seth <hseth879157@gmail.com>
2024-01-29 21:23:28 +0530

    UI changes

--- client/src/pages/Group.jsx
+++ client/src/pages/Group.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   List,
   ListItem,
@@ -8,48 +8,122 @@ import {
   Divider,
   Typography,
   Container,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Box,
 } from "@mui/material";
+import Chat from "./Chat";
 
 const Groups = () => {
-  // Dummy data for demonstration
-  const groups = [
+  const [groups, setGroups] = useState([
     { id: 1, name: "Group 1", members: 10 },
     { id: 2, name: "Group 2", members: 8 },
     { id: 3, name: "Group 3", members: 12 },
-  ];
+  ]);
+
+  const [isCreateGroupDialogOpen, setCreateGroupDialogOpen] = useState(false);
+  const [newGroupName, setNewGroupName] = useState("");
+  const [newGroupDescription, setNewGroupDescription] = useState("");
+  // Add more state variables as needed for friends list, etc.
+
+  const handleCreateGroup = () => {
+    // Add logic to create a new group and close the dialog
+    const newGroup = {
+      id: groups.length + 1,
+      name: newGroupName,
+      members: 0, // You can set an initial member count
+    };
+    setGroups([...groups, newGroup]);
+    setCreateGroupDialogOpen(false);
+    // Add any additional logic for handling friends list, etc.
+  };
 
   return (
-    <Container maxWidth="md">
-      <Typography variant="h4" gutterBottom>
-        Group List
-      </Typography>
-      <List>
-        {groups.map((group) => (
-          <React.Fragment key={group.id}>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar>{group.members}</Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary={group.name}
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      color="textPrimary"
-                    >
-                      {`${group.members} members`}
-                    </Typography>
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-            <Divider variant="inset" component="li" />
-          </React.Fragment>
-        ))}
-      </List>
-    </Container>
+    <Box>
+      <Box display="flex" flexDirection="column">
+        <Box width="100%">
+          <Button
+            sx={{ float: "right" }}
+            variant="contained"
+            color="primary"
+            onClick={() => setCreateGroupDialogOpen(true)}
+          >
+            Create New Group
+          </Button>
+        </Box>
+        <Box display="flex" flexDirection={"row"} marginTop={"30px"}>
+          <Box width="30%" marginTop={"20px"}>
+            <List marginTop={"10px"}>
+              {groups.map((group) => (
+                <React.Fragment key={group.id}>
+                  <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                      <Avatar>{group.members}</Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={group.name}
+                      secondary={
+                        <React.Fragment>
+                          <Typography
+                            component="span"
+                            variant="body2"
+                            color="textPrimary"
+                          >
+                            {`${group.members} members`}
+                          </Typography>
+                        </React.Fragment>
+                      }
+                    />
+                  </ListItem>
+                  <Divider variant="inset" component="li" />
+                </React.Fragment>
+              ))}
+            </List>
+          </Box>
+          <Box width="70%">
+            <Chat />
+          </Box>
+        </Box>
+      </Box>
+      <Dialog
+        open={isCreateGroupDialogOpen}
+        onClose={() => setCreateGroupDialogOpen(false)}
+      >
+        <DialogTitle>Create New Group</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Group Name"
+            type="text"
+            fullWidth
+            value={newGroupName}
+            onChange={(e) => setNewGroupName(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="Group Description"
+            type="text"
+            fullWidth
+            value={newGroupDescription}
+            onChange={(e) => setNewGroupDescription(e.target.value)}
+          />
+          {/* Add additional fields for friends list, etc. */}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setCreateGroupDialogOpen(false)}>
+            Cancel
+          </Button>
+          <Button onClick={handleCreateGroup} color="primary">
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
 };
 
--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,33 +1,143 @@
-import React, { useEffect } from "react";
-import { Container, CssBaseline, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Container,
+  CssBaseline,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Typography,
+  Box,
+} from "@mui/material";
+import { useDispatch } from "react-redux";
 import Post from "../components/Post";
 import { authActions } from "../_actions";
-import { useDispatch } from "react-redux";
 
 const PostSection = (props) => {
   const dispatch = useDispatch();
-  // Example post data
-  const postData = {
-    user: {
-      name: "John Doe",
-      profilePicture: "https://example.com/profile.jpg",
-    },
-    timestamp: 1643245200000, // Example timestamp (milliseconds since epoch)
-    content: "This is an example post using Material-UI in React!",
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [newPostContent, setNewPostContent] = useState("");
+
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  const handleCreatePost = () => {
+    // Add your logic to dispatch an action to create a new post
+    // Example: dispatch(authActions.createPost({ content: newPostContent }));
+
+    // Close the dialog after creating the post
+    handleDialogClose();
   };
 
   useEffect(() => {
     dispatch(authActions.getPosts());
   }, []);
 
+  const postData = [
+    {
+      user: {
+        name: "John Doe",
+        profilePicture:
+          "https://images.pexels.com/photos/19692814/pexels-photo-19692814/free-photo-of-little-monk-eye-contact.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      },
+      timestamp: 1643245200000,
+      content: "This is an example post using Material-UI in React!",
+    },
+    {
+      user: {
+        name: "John Doe",
+        profilePicture:
+          "https://images.pexels.com/photos/19692814/pexels-photo-19692814/free-photo-of-little-monk-eye-contact.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      },
+      timestamp: 1643245200000,
+      content: "This is an example post using Material-UI in React!",
+    },
+    {
+      user: {
+        name: "John Doe",
+        profilePicture:
+          "https://images.pexels.com/photos/19692814/pexels-photo-19692814/free-photo-of-little-monk-eye-contact.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      },
+      timestamp: 1643245200000,
+      content: "This is an example post using Material-UI in React!",
+    },
+    {
+      user: {
+        name: "John Doe",
+        profilePicture:
+          "https://images.pexels.com/photos/19692814/pexels-photo-19692814/free-photo-of-little-monk-eye-contact.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      },
+      timestamp: 1643245200000,
+      content: "This is an example post using Material-UI in React!",
+    },
+  ];
+
   return (
     <>
       <CssBaseline />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleDialogOpen}
+        style={{ float: "right", marginRight: "10%" }}
+      >
+        Create Post
+      </Button>
       <Container maxWidth="sm" style={{ marginTop: "2rem" }}>
         <Typography variant="h4" align="center" gutterBottom>
-          My Facebook-like Post Section
+          Post Section
         </Typography>
-        <Post {...postData} />
+
+        <Box sx={{ marginTop: "10%" }}>
+          {postData &&
+            postData.length > 0 &&
+            postData.map((post) => <Post {...post} />)}
+        </Box>
+        <Dialog open={isDialogOpen} onClose={handleDialogClose}>
+          <DialogTitle>Create a New Post</DialogTitle>
+          <DialogContent>
+            <TextField
+              label="Post Title"
+              fullWidth
+              variant="outlined"
+              value={newPostContent}
+              onChange={(e) => setNewPostContent(e.target.value)}
+              sx={{ marginTop: "15px" }}
+            />
+            <TextField
+              sx={{ marginTop: "15px" }}
+              label="Post Discreption"
+              multiline
+              rows={4}
+              fullWidth
+              variant="outlined"
+              value={newPostContent}
+              onChange={(e) => setNewPostContent(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleCreatePost}
+              style={{ marginTop: "1rem" }}
+            >
+              Post
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={handleCreatePost}
+              style={{ marginTop: "1rem", marginLeft: "1rem" }}
+            >
+              Cancle
+            </Button>
+          </DialogContent>
+        </Dialog>
       </Container>
     </>
   );
