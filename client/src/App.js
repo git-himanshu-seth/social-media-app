9f12def298ac2f877e725e8c33c0755966cf30ca
Dipti Patel <diptippatel@gmail.com>
2024-01-30 00:16:57 +0530

    persist setup done

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import "./App.css";
 import DefaultRoutes from "./rotues/routes";
 import Header from "./components/Header";
-import { store } from "./_store";
+import { store,presitStore } from "./_store";
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
     <React.StrictMode>
       <Provider store={store}>
+        <PersistGate loading={null} persistor={presitStore}>
         <div className="App">
           <Header />
           <DefaultRoutes />
         </div>
+        </PersistGate>
       </Provider>
     </React.StrictMode>
   );
--- client/src/_store/index.js
+++ client/src/_store/index.js
@@ -2,13 +2,24 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import {thunk} from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../_reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+
+
+const persistConfig = {
+  key: 'root',
+  storage: storage, // or any other storage engine
+  whitelist: ['auth'], // List of reducers to be persisted
+};
 const loogerMiddleware = createLogger();
+const persistReducers = persistReducer(persistConfig,rootReducer) 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
-    rootReducer,
+    persistReducers,
     composeEnhancer(applyMiddleware(
         thunk,
         loogerMiddleware
     ))
 );
+export const presitStore= persistStore(store);
