d71a488f17f61089028d0744cd6858da88a3c2e9
Himanshu Seth <hseth879157@gmail.com>
2024-01-29 22:16:11 +0530

    api integration changes

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import { auth } from "./auth.reducers";
 import { friend } from "./friends.reducer";
+import { group } from "./group.reducer";
+import { post } from "./posts.reducer";
 
 const appReducers = combineReducers({
   auth,
   friend,
+  group,
+  post,
 });
 
 const rootReducer = (state, action) => {
