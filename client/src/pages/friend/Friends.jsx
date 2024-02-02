f1692932443786d88482e0c42605e058d5f4cf34
Dipti Patel <diptippatel@gmail.com>
2024-02-02 15:34:53 +0530

    friend screen changes

--- client/src/pages/friend/Friends.jsx
+++ client/src/pages/friend/Friends.jsx
@@ -49,30 +49,34 @@ const Friends = () => {
       <List>
         {friendList &&
           friendList.length > 0 &&
-          friendList.map((request) => (
+          friendList.map((friend) => (
             <React.Fragment key={1}>
               <ListItem>
                 <ListItemText
-                  primary={request?.sender?.name}
-                  secondary={request?.sender?.email}
+                  primary={friend?.user?.name}
+                  secondary={friend?.user?.email}
                 />
-                <Button
-                  variant="contained"
-                  color="primary"
-                  size="small"
-                  onClick={() => handleAcceptRequest("accept")}
-                  sx={{ marginRight: "20px" }}
-                >
-                  Accept
-                </Button>
-                <Button
-                  variant="contained"
-                  color="error"
-                  size="small"
-                  onClick={() => handleAcceptRequest("reject")}
-                >
-                  Reject
-                </Button>
+                {friend?.status === "pending" && (
+                  <>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      size="small"
+                      onClick={() => handleAcceptRequest("accept")}
+                      sx={{ marginRight: "20px" }}
+                    >
+                      Accept
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="error"
+                      size="small"
+                      onClick={() => handleAcceptRequest("reject")}
+                    >
+                      Reject
+                    </Button>
+                  </>
+                )}
               </ListItem>
               <Divider />
             </React.Fragment>
